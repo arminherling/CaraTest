31e0963a0f230e585fe27c8ad326a6e9f1b46ad1
Armin Herling <armin.herling@gmail.com>
2024-08-16 19:11:57 +0200

    Cleanup

--- AalTest/source/AalTest/Diff.cpp
+++ AalTest/source/AalTest/Diff.cpp
@@ -16,11 +16,10 @@ namespace AalTest
 
         const auto prefixMismatchResult = std::mismatch(first.begin(), first.end(), second.begin(), second.end());
         const auto startIndex = std::distance(first.begin(), prefixMismatchResult.first);
-        const auto firstIsStringOffset = (first.at(first.size() - 1) == '"') ? 1 : 0;
-        const auto secondIsStringOffset = (second.at(second.size() - 1) == '"') ? 1 : 0;
 
         const auto suffixMismatchResult = std::mismatch(first.rbegin(), first.rend(), second.rbegin(), second.rend());
         const auto suffixEndIndex = std::distance(first.rbegin(), suffixMismatchResult.first) - 1;
+        const auto secondIsStringOffset = (second.at(second.size() - 1) == '"') ? 1 : 0;
         const auto endIndex = second.size() - secondIsStringOffset - suffixEndIndex;
 
         if (startIndex < endIndex)
